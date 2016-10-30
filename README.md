c2aebadc8e11456858cfc144e9764cfb4cefcc8d
pmatushkin <pmatushkin@gmail.com>
2016-10-29 17:57:35 -0700

    feat: Pass Problem 2: Construct the Suffix Array of a Long String

--- README.md
+++ README.md
@@ -3,6 +3,6 @@
 ### Problems
 Practice implementing very efficient string algorithms:
 * [`/src/kmp/KnuthMorrisPratt.java`](src/kmp/KnuthMorrisPratt.java): Problem 1, Find All Occurrences of a Pattern in a String, finds all occurrences of a pattern in a string (PASSED);
-* [`/src/suffix_array_long/SuffixArrayLong.java`](src/suffix_array_long/SuffixArrayLong.java): Problem 2, Construct the Suffix Array of a Long String, constructs the suffix array of a string;
+* [`/src/suffix_array_long/SuffixArrayLong.java`](src/suffix_array_long/SuffixArrayLong.java): Problem 2, Construct the Suffix Array of a Long String, constructs the suffix array of a string (PASSED);
 * [`/src/suffix_array_matching/SuffixArrayMatching.java`](src/suffix_array_matching/SuffixArrayMatching.java): Problem 3, Pattern Matching with the Suffix Array, finds all occurrences of a given collection of patterns in a string;
 * [`/src/suffix_tree_from_array/SuffixTreeFromArray.java`](src/suffix_tree_from_array/SuffixTreeFromArray.java): Advanced Problem 4, Construct the Suffix Tree from the Suffix Array, constructs a suffix tree from the suffix array and LCP array of a string.
--- src/suffix_array_long/SuffixArrayLong.java
+++ src/suffix_array_long/SuffixArrayLong.java
@@ -3,6 +3,8 @@ import java.io.*;
 import java.util.zip.CheckedInputStream;
 
 public class SuffixArrayLong {
+    final int ALPHABET_SIZE = 5;
+
     class FastScanner {
         StringTokenizer tok = new StringTokenizer("");
         BufferedReader in;
@@ -44,13 +46,128 @@ public class SuffixArrayLong {
     // in text where the i-th lexicographically smallest
     // suffix of text starts.
     public int[] computeSuffixArray(String text) {
-        int[] result = new int[text.length()];
+        int textLength = text.length();
+
+//        int[] result = new int[textLength];
 
         // write your code here
+        int[] result = SortCharacters(text);
+        int[] class_ = ComputeCharClasses(text, result);
+        int L = 1;
+
+        while (L < textLength) {
+            result = SortDoubled(text, L, result, class_);
+            class_ = UpdateClasses(result, class_, L);
+
+            L = 2 * L;
+        }
 
         return result;
     }
 
+    private int[] SortCharacters(String text) {
+        int textLength = text.length();
+
+        int[] result = new int[textLength];
+        int[] count = new int[ALPHABET_SIZE];
+
+        for (int i = 0; i < textLength; i++) {
+            char symbolAt = text.charAt(i);
+            int symbolIndex = letterToIndex(symbolAt);
+            count[symbolIndex] = count[symbolIndex] + 1;
+        }
+
+        for (int i = 1; i < ALPHABET_SIZE; i++) {
+            count[i] = count[i] + count[i - 1];
+        }
+
+        for (int i = textLength - 1; i >= 0; i--) {
+            char symbolAt = text.charAt(i);
+            int symbolIndex = letterToIndex(symbolAt);
+            count[symbolIndex] = count[symbolIndex] - 1;
+            result[count[symbolIndex]] = i;
+        }
+
+        return result;
+    }
+
+    private int[] ComputeCharClasses(String text, int[] order) {
+        int textLength = text.length();
+
+        int[] result = new int[textLength];
+        for (int i = 1; i < textLength; i++) {
+            if (text.charAt(order[i]) != text.charAt(order[i - 1])) {
+                result[order[i]] = result[order[i - 1]] + 1;
+            } else {
+                result[order[i]] = result[order[i - 1]];
+            }
+        }
+
+        return result;
+    }
+
+    private int[] SortDoubled(String text, int L, int[] order, int[] class_) {
+        int textLength = text.length();
+
+        int[] count = new int[textLength];
+        int[] result = new int[textLength];
+
+        for (int i = 0; i < textLength; i++) {
+            count[class_[i]] = count[class_[i]] + 1;
+        }
+
+        for (int i = 1; i < textLength; i++) {
+            count[i] = count[i] + count[i - 1];
+        }
+
+        for (int i = textLength - 1; i >= 0; i--) {
+            int start = (order[i] - L + textLength) % textLength;
+            int cl = class_[start];
+            count[cl] = count[cl] - 1;
+            result[count[cl]] = start;
+        }
+
+        return result;
+    }
+
+    private int[] UpdateClasses(int[] order, int[] class_, int L) {
+        int textLength = order.length;
+
+        int[] result = new int[textLength];
+
+        for (int i = 1; i < textLength; i++) {
+            int cur = order[i];
+            int prev = order[i - 1];
+            int mid = (cur + L) % textLength;
+            int midPrev = (prev + L) % textLength;
+
+            if ((class_[cur] != class_[prev]) || (class_[mid] != class_[midPrev])) {
+                result[cur] = result[prev] + 1;
+            } else {
+                result[cur] = result[prev];
+            }
+        }
+
+        return result;
+    }
+
+    int letterToIndex(char letter) {
+        switch (letter) {
+            case '$':
+                return 0;
+            case 'A':
+                return 1;
+            case 'C':
+                return 2;
+            case 'G':
+                return 3;
+            case 'T':
+                return 4;
+            default:
+                assert (false);
+                return -1;
+        }
+    }
 
     static public void main(String[] args) throws IOException {
         new SuffixArrayLong().run();
